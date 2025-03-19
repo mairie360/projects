61910f26382b8aff5c9b569c48d053040fad1d58
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-03-19 22:45:21 +0400

    Feat: Update the docker-compose.yml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Core",
-  description: "The web service for Core",
+  title: "Projects",
+  description: "The web service for Projects",
 };
 
 export default function RootLayout({
